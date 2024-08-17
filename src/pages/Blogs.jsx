7ac965dde2386b6364c331d2f6d44a0203d342e4
Jorge Chávez Huincho <djjmygm160399@gmail.com>
2024-08-16 19:31:09 -0500

    Creando las banners de cada sección o menú

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -3,7 +3,9 @@
 const Blogs = () => {
   return (
     <div>
-      Blogs
+      <div className="py-40 bg-black text-center text-white px-4">
+        <h2 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">Blog pages</h2>
+      </div>
     </div>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,9 @@
 const Contact = () => {
   return (
     <div>
-      
+      <div className="py-40 bg-black text-center text-white px-4">
+        <h2 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">Contact Us pages</h2>
+      </div>
     </div>
   )
 }
